54875b0158067019e4bfad26f418d219bea5c500
meise23 <bucsigabor@hotmail.com>
2026-04-06 20:00:40 +0200

    vass

--- Szak/obj/Release/net10.0/Szak.AssemblyInfo.cs
+++ Szak/obj/Release/net10.0/Szak.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Szak")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43f772adbc365180a3b3c0b88a94fcf51e06d572")]
 [assembly: System.Reflection.AssemblyProductAttribute("Szak")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Szak")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
