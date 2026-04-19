b25a41d818ca16c7732ab56cdd7e801f413cfe68
meise23 <bucsigabor@hotmail.com>
2026-04-19 16:34:04 +0200

    ff

--- Szak/obj/Debug/net10.0/Szak.AssemblyInfo.cs
+++ Szak/obj/Debug/net10.0/Szak.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Szak")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43f772adbc365180a3b3c0b88a94fcf51e06d572")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e59b744d287e7d193389bc5eded8a2c8ee2d9c1d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Szak")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Szak")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
